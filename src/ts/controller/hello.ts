0af094fbecd01f48e5727fa13987da73750106b6
LittleKey <kunyu.liao@qianmo-inc.com>
2016-05-25 17:49:19 +0800

    add httpService to HelloController

--- src/ts/controller/hello.ts
+++ src/ts/controller/hello.ts
@@ -4,12 +4,16 @@
 module App {
   export module Ctrl {
     export class Hello {
-      static $inject = ['$scope', '$routeParams'];
+      static $inject = ['$scope', '$routeParams', '$http'];
       constructor(
           private $scope: ng.IScope,
-          private $routeParams: ng.route.IRouteParamsService) {
+          private $routeParams: ng.route.IRouteParamsService,
+          private $http: ng.IHttpService) {
           let name = $routeParams['name'];
           $scope['name'] = name ? name : 'world (default)';
+          $http.get("/index.html").then(function (response: any) {
+            $scope['name'] = response.status;
+          });
       }
     }
   }
